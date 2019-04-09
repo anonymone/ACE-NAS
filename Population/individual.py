1cd264af7e4aa01136372d14e874cb175f91e3ba
severuspeng <pengshichenno.1@live.com>
2019-04-09 17:16:11 +0800

    change the folder structure

--- Population/individual.py
+++ Population/individual.py
@@ -5,7 +5,8 @@ class individual():
     '''
     This is a basic individual class of all solution.
     '''
-    def __init__(self,config, arg = None):
+
+    def __init__(self, config, arg=None):
         '''
         self.__dec is numpy type
         self.__fitness is numpy type
@@ -16,29 +17,30 @@ class individual():
             assert type(arg) == dict, 'arg is not dict type.'
             for key in arg:
                 self.__dict__[key] = arg[key]
-    
+
     def get_dec(self):
         return self.dec.copy()
 
     def get_fitness(self):
         return self.fitness.copy()
-    
+
     def set_dec(self, dec):
         self.dec = dec.copy()
-    
-    def set_fitness(self,fitness):
+
+    def set_fitness(self, fitness):
         self.fitness = fitness.copy()
 
 
 class SEA_individual(individual):
-    def __init__(self,config):
+    def __init__(self, config):
         # load setting
-        super(SEA_individual,self).__init__(config=config) 
+        super(SEA_individual, self).__init__(config=config)
         self.codeLength = int(config['codeLength'])
+        self.blockLength = int(config['blockLength'])
         self.codeType = config['codeType']
         self.codeMaxValue = int(config['codeMaxValue'])
         self.fitnessSize = int(config['objectiveNum'])
-        #initiate decision variables
-        self.dec = np.random.random_integers(1,self.codeMaxValue,size=(1,self.codeLength))
-        self.fitness = np.zeros((1,self.fitnessSize))
-
+        # initiate decision variables
+        self.dec = np.random.random_integers(
+            1, self.codeMaxValue, size=(1, self.codeLength*self.blockLength))
+        self.fitness = np.zeros((1, self.fitnessSize))
---
+++ Population/population.py
@@ -0,0 +1,20 @@
+import individual
+
+
+class population():
+    def __init__(self, config, arg=None):
+        self.popSize = int(config['population setting']['popSize'])
+        self.generation = dict()
+        # initiate generation
+        self.generation['0'] = [individual.SEA_individual(
+            config['individual setting'] for x in range(self.popSize))]
+
+    def get_population(self, index=-1):
+        if index == -1:
+            index = str(len(self.generation)-1)
+        return self.generation[index]
+
+    def add_population(self, newPop):
+        if len(newPop) != self.popSize:
+            print('The new population size is not suit rule')
+        self.generation[str(len(self.pop))] = newPop
