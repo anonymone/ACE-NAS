25b41095fd81ff933938efe5e9d03406eb017561
severuspeng <pengshichenno.1@live.com>
2019-04-22 18:59:14 +0800

    fix bugs in NSGA2

--- EvolutionAlgorithm/EAbase.py
+++ EvolutionAlgorithm/EAbase.py
@@ -1,4 +1,4 @@
-class EABase():
+class EAbase():
     def __init__(self):
         pass
 
--- EvolutionAlgorithm/NSGA2/NSGA2.py
+++ EvolutionAlgorithm/NSGA2/NSGA2.py
@@ -1,20 +1,21 @@
-import EAbase as EAbase
+import EAbase
 from Engine import StateControl
 
 import random
+import copy
 import numpy as np
 
 
-class NSGA2(EABase):
+class NSGA2(EAbase.EAbase):
     def __init__(self, config):
         super(NSGA2, self).__init__()
-        self.mutateRate = int(config['EA setting']['mutateRate'])
-        self.crossOver = bool(config['EA setting']['crossOver'])
+        self.mutateRate = float(config['EA setting']['mutateRate'])
+        self.isCrossOver = bool(config['EA setting']['crossOver'])
         self.mutateType = config['EA setting']['codeType']
-        self.mutateGenerate = np.randint(1,9)
+        self.mutateGenerate = np.random.randint(1, 9)
 
     def mutate(self, pop, mutateFunc=None):
-        newPop = pop.copy()
+        newPop = copy.deepcopy(pop)
         (popLength, fitnessNum) = newPop[1]
         if mutateFunc is None:
             for ind in newPop[0]:
@@ -24,11 +25,10 @@ class NSGA2(EABase):
         return newPop
 
     def crossOver(self, ind1, ind2):
-        cutPoint = np.randint(2, len(ind1)-2)
-        (sub1,sub2) = (ind1[0][0:cutPoint], ind1[0][cutPoint:])
-        (sub3,sub4) = (ind2[0][0:cutPoint], ind2[0][cutPoint:])
-        return (np.hstack(sub1,sub4),np.hstack(sub3,sub2))
-
+        cutPoint = np.random.randint(2, ind1.shape[0]-2)
+        (sub1, sub2) = (ind1[0:cutPoint], ind1[cutPoint:])
+        (sub3, sub4) = (ind2[0:cutPoint], ind2[cutPoint:])
+        return np.vstack([np.hstack([sub1, sub4]), np.hstack([sub3, sub2])])
 
     def crowdingDistance(self, pop):
         pass
