9bc0a650c6378036b38f08195f07910dcab21bc0
severuspeng <pengshichenno.1@live.com>
2019-05-28 18:18:47 +0800

    add decoder code

---
+++ Model/actionInstruction.py
@@ -0,0 +1,5 @@
+class Action:
+    def __init__(self):
+        self.ADD_EDGE = 0
+        self.ADD_NODE = 1
+        
--- Model/layers.py
+++ Model/layers.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from copy import copy
 from collections import OrderedDict
 
+from actionInstruction import Action
+
 
 # Basic Node Type
 class ConvNode(nn.Module):
@@ -20,8 +22,8 @@ class ConvNode(nn.Module):
         padding = int((kernelSize-1)/2)
         self.model = nn.Sequential(
             nn.Conv2d(in_channels=inChannels, out_channels=outChannels,
-                      kernel_size=kernelSize, stride=stride, padding=padding, bias=bias)
-            nn.BatchNorm2d(outChannels)
+                      kernel_size=kernelSize, stride=stride, padding=padding, bias=bias),
+            nn.BatchNorm2d(outChannels),
             nn.ReLU(inplace=True)
         )
 
@@ -47,15 +49,43 @@ class PoolNode(nn.Module):
         return self.model(x)
 
 class SEENetworkGenerator(nn.Module):
-    def __init__(self, code, repeat=None):
+    def __init__(self, code, inChannel, outChannel, repeat=None):
         super(SEENetworkGenerator, self).__init__()
+        self.nodeList = nn.ModuleList()
+        self.nodeGenerator = ConvNode
+        self.node_graph = self.decoder(code)
+        self.actionIns = Action()
         
-    @staticmethod
     def decoder(self, code):
-        pass
-    
+        node = []
+        node_graph = dict()
+        backbone, actionCode = code[0],code[1:]
+        # build Backbone
+        _,numberOfNode, nodeType = backbone[0],backbone[1],backbone[2]
+        for i in range(numberOfNode-1):
+            if i not in node_graph.keys():
+                node_graph[i] = list()
+            node_graph[i].append(i)
+            node.append(1)
+        # build topological structure
+        actioncode_iterator = iter(actionCode)
+        for i in range(int(len(actionCode)/2)):
+            # build graph
+            From, Action, To = actioncode_iterator.__next__()
+            if Action == self.actionIns.ADD_EDGE:
+                node_graph[To%len(node_graph)].append(Action%len(node_graph))
+            elif Action == self.actionIns.ADD_NODE:
+                node_graph[len(node_graph)] = [From]
+                node_graph[to].append(len(node_graph))
+            else:
+                raise Exception('Unknown action code.')
+                
     def getModle(self):
         pass
     
     def forward(self,x):
         pass
+    
+if __name__ == "__main__":
+    a = SEENetworkGenerator('code')
+    print('hello')
