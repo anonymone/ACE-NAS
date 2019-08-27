5c65f1c974020091101ee3727ad14426a1a6523e
severuspeng <pengshichenno.1@live.com>
2019-08-27 17:09:24 +0800

    add encoder Model and surrogate Model

--- Model/embeddingModel.py
+++ Model/embeddingModel.py
@@ -1,9 +1,11 @@
+import sys
 import os
 import argparse
 import logging
 import torch
 from torch.optim.lr_scheduler import StepLR
 import torchtext
+import numpy as np
 
 import seq2seq
 from seq2seq.trainer import SupervisedTrainer
@@ -14,117 +16,116 @@ from seq2seq.dataset import SourceField, TargetField
 from seq2seq.evaluator import Predictor
 from seq2seq.util.checkpoint import Checkpoint
 
-# Used in jupyter lab
-# import sys; sys.argv=['']; del sys
+log_format = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                        format=log_format, datefmt='%m/%d %I:%M:%S %p')
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--train_path', action='store', dest='train_path', default='./data.txt',
-                    help='Path to train data')
-parser.add_argument('--dev_path', action='store', dest='dev_path', default='./data_val.txt',
-                    help='Path to dev data')
-parser.add_argument('--expt_dir', action='store', dest='expt_dir', default='./seq2seqModel',
-                    help='Path to experiment directory. If load_checkpoint is True, then path to checkpoint directory has to be provided')
-parser.add_argument('--load_checkpoint', action='store', dest='load_checkpoint',
-                    help='The name of the checkpoint to load, usually an encoded time string')
-parser.add_argument('--resume', action='store_true', dest='resume',
-                    default=False,
-                    help='Indicates if training has to be resumed from the latest checkpoint')
-parser.add_argument('--log-level', dest='log_level',
-                    default='info',
-                    help='Logging level.')
+device = 'cuda' if torch.cuda.is_available() else "cpu"
 
-opt = parser.parse_args()
-
-LOG_FORMAT = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
-logging.basicConfig(format=LOG_FORMAT, level=getattr(logging, opt.log_level.upper()))
-logging.info(opt)
-
-def GetEmbeddingModel(opt = opt):
-    if opt.load_checkpoint is not None:
-        logging.info("loading checkpoint from {}".format(os.path.join(opt.expt_dir, Checkpoint.CHECKPOINT_DIR_NAME, opt.load_checkpoint)))
-        checkpoint_path = os.path.join(opt.expt_dir, Checkpoint.CHECKPOINT_DIR_NAME, opt.load_checkpoint)
+class EmbeddingModel:
+    def __init__(self, opt):
+        logging.info("loading checkpoint from {}".format(os.path.join(opt.EmdeddingExptDir, Checkpoint.CHECKPOINT_DIR_NAME, opt.EmdeddingLoadCheckpoint)))
+        checkpoint_path = os.path.join(opt.EmdeddingExptDir, Checkpoint.CHECKPOINT_DIR_NAME, opt.EmdeddingLoadCheckpoint)
         checkpoint = Checkpoint.load(checkpoint_path)
-        seq2seq = checkpoint.model
-        input_vocab = checkpoint.input_vocab
-        output_vocab = checkpoint.output_vocab
-    else:
-        # Prepare dataset
-        src = SourceField()
-        tgt = TargetField()
-        max_len = 90
-        def len_filter(example):
-            return len(example.src) <= max_len and len(example.tgt) <= max_len
-        train = torchtext.data.TabularDataset(
-            path=opt.train_path, format='tsv',
-            fields=[('src', src), ('tgt', tgt)],
-            filter_pred=len_filter
-        )
-        dev = torchtext.data.TabularDataset(
-            path=opt.dev_path, format='tsv',
-            fields=[('src', src), ('tgt', tgt)],
-            filter_pred=len_filter
-        )
-        src.build_vocab(train, max_size=1000)
-        tgt.build_vocab(train, max_size=1000)
-        input_vocab = src.vocab
-        output_vocab = tgt.vocab
+        self.seq2seq = checkpoint.model
+        self.input_vocab = checkpoint.input_vocab
+        self.output_vocab = checkpoint.output_vocab
 
-        # NOTE: If the source field name and the target field name
-        # are different from 'src' and 'tgt' respectively, they have
-        # to be set explicitly before any training or inference
-        # seq2seq.src_field_name = 'src'
-        # seq2seq.tgt_field_name = 'tgt'
+    def encode(self, code):
+        '''
+        Inputs: a string array, each row is an input sequence.
+        Outputs: a numpy array, each row is an output vector.
+        '''
+        vectors = []
+        for seqStr in code:
+            seq = seqStr.strip().split()
+            src_id_seq = torch.LongTensor([self.input_vocab.stoi[tok] for tok in seq]).view(1,-1)
+            src_id_seq = src_id_seq.to(device)
+            with torch.no_grad():
+                output, hiden = seq2seq.encoder(src_id_seq, [len(seq)])
+            hiden_cpu = hiden.cpu()
+            vectors.append(np.append(hiden_cpu.data.numpy().reshape(1,-1), value))
+        return np.array(vectors)
 
-        # Prepare loss
-        weight = torch.ones(len(tgt.vocab))
-        pad = tgt.vocab.stoi[tgt.pad_token]
-        loss = Perplexity(weight, pad)
+
+def trainEmbeddingModel(opt):
+    # Prepare dataset
+    src = SourceField()
+    tgt = TargetField()
+    max_len = 90
+    def len_filter(example):
+        return len(example.src) <= max_len and len(example.tgt) <= max_len
+    train = torchtext.data.TabularDataset(
+        path=opt.train_path, format='tsv',
+        fields=[('src', src), ('tgt', tgt)],
+        filter_pred=len_filter
+    )
+    dev = torchtext.data.TabularDataset(
+        path=opt.dev_path, format='tsv',
+        fields=[('src', src), ('tgt', tgt)],
+        filter_pred=len_filter
+    )
+    src.build_vocab(train, max_size=1000)
+    tgt.build_vocab(train, max_size=1000)
+    input_vocab = src.vocab
+    output_vocab = tgt.vocab
+
+    # NOTE: If the source field name and the target field name
+    # are different from 'src' and 'tgt' respectively, they have
+    # to be set explicitly before any training or inference
+    # seq2seq.src_field_name = 'src'
+    # seq2seq.tgt_field_name = 'tgt'
+
+    # Prepare loss
+    weight = torch.ones(len(tgt.vocab))
+    pad = tgt.vocab.stoi[tgt.pad_token]
+    loss = Perplexity(weight, pad)
+    if torch.cuda.is_available():
+        loss.cuda()
+
+    seq2seq = None
+    optimizer = None
+    if not opt.resume:
+        # Initialize model
+        hidden_size=128
+        bidirectional = True
+        encoder = EncoderRNN(len(src.vocab), max_len, hidden_size,
+                            bidirectional=bidirectional, variable_lengths=True)
+        decoder = DecoderRNN(len(tgt.vocab), max_len, hidden_size * 2 if bidirectional else hidden_size,
+                            dropout_p=0.2, use_attention=True, bidirectional=bidirectional,
+                            eos_id=tgt.eos_id, sos_id=tgt.sos_id)
+        seq2seq = Seq2seq(encoder, decoder)
         if torch.cuda.is_available():
-            loss.cuda()
+            seq2seq.cuda()
 
-        seq2seq = None
-        optimizer = None
-        if not opt.resume:
-            # Initialize model
-            hidden_size=128
-            bidirectional = True
-            encoder = EncoderRNN(len(src.vocab), max_len, hidden_size,
-                                bidirectional=bidirectional, variable_lengths=True)
-            decoder = DecoderRNN(len(tgt.vocab), max_len, hidden_size * 2 if bidirectional else hidden_size,
-                                dropout_p=0.2, use_attention=True, bidirectional=bidirectional,
-                                eos_id=tgt.eos_id, sos_id=tgt.sos_id)
-            seq2seq = Seq2seq(encoder, decoder)
-            if torch.cuda.is_available():
-                seq2seq.cuda()
+        for param in seq2seq.parameters():
+            param.data.uniform_(-0.08, 0.08)
 
-            for param in seq2seq.parameters():
-                param.data.uniform_(-0.08, 0.08)
+        # Optimizer and learning rate scheduler can be customized by
+        # explicitly constructing the objects and pass to the trainer.
+        #
+        # optimizer = Optimizer(torch.optim.Adam(seq2seq.parameters()), max_grad_norm=5)
+        # scheduler = StepLR(optimizer.optimizer, 1)
+        # optimizer.set_scheduler(scheduler)
 
-            # Optimizer and learning rate scheduler can be customized by
-            # explicitly constructing the objects and pass to the trainer.
-            #
-            # optimizer = Optimizer(torch.optim.Adam(seq2seq.parameters()), max_grad_norm=5)
-            # scheduler = StepLR(optimizer.optimizer, 1)
-            # optimizer.set_scheduler(scheduler)
+    # train
+    t = SupervisedTrainer(loss=loss, batch_size=96,
+                        checkpoint_every=1000,
+                        print_every=1000, expt_dir=opt.expt_dir)
 
-        # train
-        t = SupervisedTrainer(loss=loss, batch_size=96,
-                            checkpoint_every=1000,
-                            print_every=1000, expt_dir=opt.expt_dir)
+    seq2seq = t.train(seq2seq, train,
+                    num_epochs=20, dev_data=dev,
+                    optimizer=optimizer,
+                    teacher_forcing_ratio=0.5,
+                    resume=opt.resume)
+# try:
+#     raw_input          # Python 2
+# except NameError:
+#     raw_input = input  # Python 3
 
-        seq2seq = t.train(seq2seq, train,
-                        num_epochs=20, dev_data=dev,
-                        optimizer=optimizer,
-                        teacher_forcing_ratio=0.5,
-                        resume=opt.resume)
-    # try:
-    #     raw_input          # Python 2
-    # except NameError:
-    #     raw_input = input  # Python 3
+# predictor = Predictor(seq2seq, input_vocab, output_vocab)
 
-    # predictor = Predictor(seq2seq, input_vocab, output_vocab)
-
-    # while True:
-    #     seq_str = raw_input("Type in a source sequence:")
-    #     seq = seq_str.strip().split()
-    #     print(predictor.predict(seq))
+# while True:
+#     seq_str = raw_input("Type in a source sequence:")
+#     seq = seq_str.strip().split()
+#     print(predictor.predict(seq))
--- Model/surroogate.py
+++ Model/surroogate.py
@@ -1,9 +1,20 @@
+import sys
+# update your projecty root path before running
+sys.path.insert(0, './')
+
 import numpy as np
 import torch 
 import torch.nn as nn
+import torch.utils.data as data
+import itertools
+import random 
+
+from misc import utils
+from Model import embeddingModel
+
 
 class RankNet(nn.Module):
-    def __init__(self, sizeList=[(256,128),(128,64),(64,32),(32,2)]):
+    def __init__(self, sizeList=[(256,128),(128,64),(64,32),(32,1)]):
         super(RankNet,self).__init__()
         self.model = nn.Sequential()
         for inSize, outSize in sizeList[:-1]:
@@ -20,38 +31,123 @@ class RankNet(nn.Module):
     def predict(self, inputs):
         return self.model(inputs)
 
-
-if __name__ == "__main__":
-    model = RankNet()
-    criterion = nn.NLLLoss()
-
-    parameters = filter(lambda p: p.requires_grad, model.parameters())
-    optimizer = torch.optim.Adam(parameters)
-    # dataset 
-    data = np.random.rand(1000,257).astype(dtype="float32")
-    print(data[0:10,:])
-
-    y_i = data[0:500,-1]
-    y_j = data[500:1000,-1]
-
-    labels = []
-    for y in zip(y_i,y_j):
-        if y[0] >= y[1]:
-            labels.append(1)
+class RankNetDataset(data.Dataset):
+    def __init__(self, dataNumpy, train=True,
+                 transform=None, target_transferm=None):
+        self.transform = transform
+        self.target_transform = target_transferm
+        self.train = train
+        self.dataset = RankNetDataset.batchData(dataNumpy.shape[0])
+        if self.train:
+            self.train_data = dataNumpy[:,1:-1].astype(dtype="float32")
+            self.train_values = dataNumpy[:,-1].astype(dtype="float32")
         else:
-            labels.append(0)
-    #    else:
-    #        labels.append(0.5)
-    labels = np.array(labels, dtype="int64")
-    print(labels)
-    data = torch.from_numpy(data)
-    labels = torch.from_numpy(labels)
-    outputs = model(data[0:500,:-1],data[500:1000,:-1])
-    np.sum(np.argmax(outputs.data.numpy(),1) == labels.data.numpy())/500
-    for i in range(200):
-        optimizer.zero_grad()
-        outputs = model(data[0:500,:-1],data[500:1000,:-1])
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-    print(np.sum(np.argmax(model(data[0:500,:-1],data[500:1000,:-1]).data.numpy(),1) == labels.data.numpy())/500)
+            self.test_data = dataNumpy[:,1:-1].astype(dtype="float32")
+            self.test_values = dataNumpy[:,-1].astype(dtype="float32")
+    
+    @staticmethod
+    def batchData(datasetSize, batchSize = 32):
+        index = [x for x in range(datasetSize)]
+        pairs = [np.array([i,j]) for i,j in itertools.product(index,index)]
+        random.shuffle(pairs)
+        # pairs = [pairs[i:i+batchSize] for i in range(0,datasetSize - datasetSize%batchSize, batchSize)]
+        return np.array(pairs)
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        index_ij = self.dataset[index]
+        target = np.zeros(1)
+        if self.train:
+            vector_i, value_i = self.train_data[index_ij[0]], self.train_values[index_ij[0]]
+            vector_j, value_j = self.train_data[index_ij[1]], self.train_values[index_ij[1]]            
+        else:
+            vector_i, value_i = self.test_data[index_ij[0]], self.train_values[index_ij[0]]
+            vector_j, value_j = self.test_data[index_ij[1]], self.train_values[index_ij[1]]
+        
+        target = np.ones(1) if value_i >= value_j else np.zeros(1)
+        
+        if self.transform is not None:
+            vector = self.transform(vector)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return (vector_i,vector_j), target.astype("float32")
+
+    def __len__(self):
+        if self.train:
+            return len(self.dataset)
+        else:
+            return len(self.dataset)
+
+    def __repr__(self):
+        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
+        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
+        tmp = 'train' if self.train is True else 'test'
+        fmt_str += '    Split: {}\n'.format(tmp)
+        # fmt_str += '    Root Location: {}\n'.format(self.root)
+        tmp = '    Transforms (if any): '
+        fmt_str += '{0}{1}\n'.format(tmp, self.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        tmp = '    Target Transforms (if any): '
+        fmt_str += '{0}{1}'.format(tmp, self.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        return fmt_str
+
+class Predictor:
+    def __init__(self, encoder, modelSize = [(256,128),(128,64),(64,32),(32,1)]):
+        self.model = RankNet(modelSize)
+        self.criterion = nn.BCELoss()
+        parameters = filter(lambda p: p.requires_grad, self.model.parameters())
+        self.optimizer = torch.optim.Adam(parameters)
+        self.encoder = encoder
+        
+    def trian(self, dataset, trainEpoch = 50, printFreqence=1000):
+        device = "cuda" if torch.cuda.is_available() else "cpu" 
+        self.model = self.model.to(device)
+
+        # Dataset Loader
+        dataQueue = torch.utils.data.DataLoader(dataset=dataset, 
+                                                batch_size=32, 
+                                                shuffle=True)
+        for epoch in range(trainEpoch):
+            train_loss = 0
+            correct = 0
+            total = 0
+            for step, (inputs, labels) in enumerate(dataQueue):
+
+                inputs_i,inputs_j, labels = inputs[0].to(device), inputs[1].to(device), labels.to(device)
+                self.optimizer.zero_grad()
+                outputs = self.model(inputs_i, inputs_j)
+                loss = self.criterion(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+
+                train_loss += loss.item()
+                outputs = outputs.cpu()
+                predicted = outputs.detach().numpy()
+                predicted[predicted>=0.5] = 1
+                predicted[predicted<0.5] = 0
+                total += labels.size(0)
+                correct += np.sum(predicted.reshape(1,-1)==labels.cpu().numpy().reshape(1,-1))
+                if (step+1)%printFreqence == 0:
+                    print("Epoch: {0}, Step: {1} Loss: {2}, Acc: {3}".format(epoch, 
+                                                                            step, 
+                                                                            train_loss/total, 
+                                                                            100.*correct/total))
+
+    def predict(self):
+        pass
+        
+if __name__ == "__main__":
+    import pandas as pd
+    import numpy as np
+    import torch.utils as utils
+
+    dataset = pd.read_csv('./Dataset/encodeData/surrogate.txt')
+    dataset = RankNetDataset(dataset.values)
+
+    predictor = Predictor(None)
+    predictor.trian(dataset=dataset)    
