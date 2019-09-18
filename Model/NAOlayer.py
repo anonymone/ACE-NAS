fix bugs in gradient lossing.

Model/NAOlayer.py
@@ -48,14 +48,14 @@ class SepConv(nn.Module):
         self.C_in = C_in
         self.padding = padding
 
-        self.relu1 = nn.ReLU(inplace=True)
+        self.relu1 = nn.ReLU(inplace=False)
         self.W1_depthwise = nn.Conv2d(
             C_in, C_out, kernel_size=kernel_size, stride=stride, padding=self.padding, groups=self.C_in)
         self.W1_pointwise = nn.Conv2d(
             C_in, C_out, kernel_size=1, padding=0, groups=self.C_in)
         self.bn1 = nn.BatchNorm2d(C_out)
 
-        self.relu2 = nn.ReLU(inplace=True)
+        self.relu2 = nn.ReLU(inplace=False)
         self.W2_depthwise = nn.Conv2d(
             C_in, C_out, kernel_size=kernel_size, stride=1, padding=self.padding, groups=self.C_in)
         self.W2_pointwise = nn.Conv2d(
@@ -95,7 +95,7 @@ class SEECell(nn.Module):
         self.actionIns = Action()
         self.nodeGraph = self.decoder(code)
         _, self.topoList = utils.isLoop(self.nodeGraph)
-        self.preOps = self.getPreOps()
+        self.preOps = nn.ModuleList(self.getPreOps())
         # self.layerId = layerId
         # self.layers = layers
         # self.steps = steps
@@ -126,7 +126,7 @@ class SEECell(nn.Module):
             if len(preNode) > 1:
                 conv1x1Nodes[node] = nn.Conv2d(
                     len(preNode)*self.channels, self.channels, kernel_size=1, bias=False)
-        return nn.ModuleList(conv1x1Nodes)
+        return conv1x1Nodes
 
     def decoder(self, code):
         '''

Search/trainSearch.py
@@ -21,6 +21,7 @@ from misc import utils
 from misc.flops_counter import add_flops_counting_methods
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+# torch.autograd.set_detect_anomaly(True)
 
 
 def main(code, args, complement=False, **kwargs):
@@ -143,12 +144,12 @@ def main(code, args, complement=False, **kwargs):
     train_queue = torch.utils.data.DataLoader(
         train_data, batch_size=batch_size,
         # sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[:split]),
-        pin_memory=True, num_workers=4)
+        pin_memory=True, num_workers=0)
 
     valid_queue = torch.utils.data.DataLoader(
         valid_data, batch_size=batch_size,
         # sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[split:num_train]),
-        pin_memory=True, num_workers=4)
+        pin_memory=True, num_workers=0)
 
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
         optimizer, int(epochs))

misc/utils.py
@@ -175,15 +175,15 @@ def isLoop(graph, newEdge=None):
 class ReLUConvBN(nn.Module):
     def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
         super(ReLUConvBN, self).__init__()
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace=False)
         self.conv = nn.Conv2d(C_in, C_out, kernel_size, stride=stride, padding=padding, bias=False)
         self.bn = nn.BatchNorm2d(C_out, affine=affine)
     
-    def forward(self, x, bn_train=False):
+    def forward(self, x):
         x = self.relu(x)
         x = self.conv(x)
-        if bn_train:
-            self.bn.train()
+        # if bn_train:
+        #     self.bn.train()
         x = self.bn(x)
         return x
 
@@ -220,7 +220,7 @@ class MaybeCalibrateSize(nn.Module):
         # previous reduction cell
         if hw[0] != hw[1]:
             assert hw[0] == 2 * hw[1]
-            self.relu = nn.ReLU(inplace=True)
+            self.relu = nn.ReLU(inplace=False)
             self.preprocess_x = FactorizedReduce(c[0], channels, affine)
             x_out_shape = [hw[1], hw[1], channels]
             self.multi_adds += 1 * 1 * c[0] * channels * hw[1] * hw[1]
@@ -235,28 +235,29 @@ class MaybeCalibrateSize(nn.Module):
             
         self.out_shape = [x_out_shape, y_out_shape]
     
-    def forward(self, s0, s1, bn_train=False):
+    def forward(self, s0, s1):
         if s0.size(2) != s1.size(2):
             s0 = self.relu(s0)
-            s0 = self.preprocess_x(s0, bn_train=bn_train)
+            s0 = self.preprocess_x(s0)
         elif s0.size(1) != self.channels:
-            s0 = self.preprocess_x(s0, bn_train=bn_train)
+            s0 = self.preprocess_x(s0)
         if s1.size(1) != self.channels:
-            s1 = self.preprocess_y(s1, bn_train=bn_train)
-        return s0+s1
+            s1 = self.preprocess_y(s1)
+        out = torch.add(s0,s1)
+        return out
 
 class AuxHeadCIFAR(nn.Module):
     def __init__(self, C_in, classes):
         """assuming input size 8x8"""
         super(AuxHeadCIFAR, self).__init__()
-        self.relu1 = nn.ReLU(inplace=True)
+        self.relu1 = nn.ReLU(inplace=False)
         self.avg_pool = nn.AvgPool2d(5, stride=3, padding=0, count_include_pad=False)
         self.conv1 = nn.Conv2d(C_in, 128, 1, bias=False)
         self.bn1 = nn.BatchNorm2d(128)
-        self.relu2 = nn.ReLU(inplace=True)
+        self.relu2 = nn.ReLU(inplace=False)
         self.conv2 = nn.Conv2d(128, 768, 2, bias=False)
         self.bn2 = nn.BatchNorm2d(768)
-        self.relu3 = nn.ReLU(inplace=True)
+        self.relu3 = nn.ReLU(inplace=False)
         self.classifier = nn.Linear(768, classes)
         
     def forward(self, x, bn_train=False):