cff97fb2ac8570078a1591f5abe188a887e251bd
severuspeng <pengshichenno.1@live.com>
2019-10-08 17:16:48 +0800

    add shell script

--- search.sh
+++ search.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
+nvidia-smi
 
-python3 Search/evolutionSearch.py --save SEE_seed0 \
-                                  --seed 0 \
-                                  --generation 30 \
-                                  --EmbeddingLoadCheckpoint 2019_08_26_07_35_34 \
-                                  --PredictorModelEpoch 20 \
-                                  --PredictorModelPath ./Dataset/encodeData/RankModel/ \
-                                  --popSize 30 \
-                                  --mutationRate 0.8 \
-                                  --trainSearch_epoch 30 \
-                                  --trainSearch_initChannel 32 \
-                                  --trainSearchDataset cifar10 \
-                                  --trainSearchDatasetClassNumber 10 \
-                                  --trainSearchSurrogate 5 \
-                                  --evalMode DEBUG
+Embedding_Checke_Point_Path=2019_08_26_07_35_34
+evalMode=DEBUG
+
+python3 Search/evolutionSearch.py --save=Run_$evalMode \
+                                  --seed=0 \
+                                  --generation=30 \
+                                  --Embedding_LoadCheckpoint=$Embedding_Checke_Point_Path \
+                                  --PredictorModelEpoch=20 \
+                                  --PredictorModelPath=./Dataset/encodeData/RankModel/ \
+                                  --popSize=30 \
+                                  --mutationRate=0.8 \
+                                  --trainSearch_epoch=30 \
+                                  --trainSearch_initChannel=32 \
+                                  --trainSearchDataset=cifar10 \
+                                  --trainSearchDatasetClassNumber=10 \
+                                  --trainSearchSurrogate=5 \
+                                  --evalMode=$evalMode
