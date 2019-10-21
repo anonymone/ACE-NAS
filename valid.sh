09dacb44da642f27d906576dc46255dd35c2a838
anonymone <pengshichenno.1@live.com>
2019-10-21 02:00:50 +0000

    fix bug in valid.sh

--- valid.sh
+++ valid.sh
@@ -4,7 +4,7 @@ nvidia-smi
 Embedding_Checke_Point_Path=2019_08_26_07_35_34
 search_space=Node_Cell
 
-python3 Validation/trainCifar.py  --code_str=Phase:565-942-627-465-742-441-262-663-208-711-065-861-284-788-325-Phase:645-703-557-802-760-512-294-012-232-585-161-526-622-286-341
+python3 Validation/trainCifar.py  --code_str=Phase:565-942-627-465-742-441-262-663-208-711-065-861-284-788-325-Phase:645-703-557-802-760-512-294-012-232-585-161-526-622-286-341 \
                                   --save=ValidationCifar_$search_space \
                                   --seed=0 \
                                   --data_worker=12 \
